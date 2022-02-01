e8e082902743fc7c00388d6f5cf86ed3c9f671be
Grzegorz Oleksa <grzesieq92@gmail.com>
2022-02-01 13:15:07 +0100

    js code changed

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-console.log("Cześć! Fajnie, że tu jesteś. :)");
-let button = document.querySelector(".js-header__button");
-let body = document.querySelector(".js-body");
-let themeName = document.querySelector(".js-themeName"); 
-
-button.addEventListener("click", () => {
-    body.classList.toggle("body--dark");
-
-    themeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
-
-
-});
+{
+    const welcome = () => {
+        console.log("Cześć! Fajnie, że tu jesteś. :)");
+    }
+    const toggleBackground = () => {
+        const body = document.documentElement;
+        const themeName = document.querySelector(".js-themeName")
+        body.classList.toggle("body--dark");
+        themeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
+    };
+    const init = () => {
+        const header__button = document.querySelector(".js-header__button");
+        header__button.addEventListener("click", toggleBackground);
+        welcome();
+    };
+    init();
+}
